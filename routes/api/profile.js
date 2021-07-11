0f5fd3c5de18bc32965eac44c0bfd07553a279cc
iamjustnobody <nobodyiamjustnobody@outlook.com>
2021-07-11 21:07:27 +0100

    add/delete/update profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,8 +21,8 @@ router.get('/me',authmw,async (req,res)=>{
         
         if(!userProfile) return res.status(400).json({noProfExistsMsg:'There is no profile for this user'})
 
-        res.status(200).json(profile) //or res.json(profile) //return opt
-        //return res.status(200).json({profile})//return opt 
+        res.status(200).json(userProfile) //or res.json(userProfile) //return opt
+        //return res.status(200).json({userProfile})//return opt 
         //all ok
     }catch(err){
         console.error(err.message);
@@ -71,7 +71,8 @@ router.post(
         try{
             let profile=await Profile.findOne({userid:req.user.id})
             if(profile){//UPDATE - amendnp
-                profile=await Profile.findOneAndUpdate({userid:req.user.id},{$set:profileFields},{new:true});
+                profile=await Profile.findOneAndUpdate({userid:req.user.id},{$set:profileFields},{new:true});//ok
+               // profile=await Profile.findOneAndUpdate({userid:req.user.id},{profileFields},{new:true}); //ok too
                 return res.json(profile) //or return res.status(200).json({profile}) //must have 'return' for res
             }
 
@@ -133,8 +134,11 @@ router.get('/user/:user_id',async(req,res)=>{
 //like GET api/profile/me & POST api/profile
 router.delete('/',authmw,async(req,res)=>{
     try {
-        await Profile.findOneAndRemove({userid:req.user.id});
-        await User.findOneAndRemove({_id:req.user.id}); //_id:ok - did remove; id: did NOT remove
+        await Profile.findOneAndRemove({userid:req.user.id});//or findOneAndDelete
+        await User.findOneAndRemove({_id:req.user.id}); //_id:ok - did remove; id: did NOT remove 
+        //same for findOneAndDelete
+        //await User.findByIdAndRemove(req.user.id);//ok
+        //await User.findByIdAndDelete(req.user.id);//ok
 
         res.json({rmMsg:'User removed'});
     } catch (error) {
@@ -142,6 +146,8 @@ router.delete('/',authmw,async(req,res)=>{
         res.status(500).send('Server Error')
     }
 })
+
+
 
 
 //PUT api/profile/experience --- ADD experience to authorised/loggedin user profile/experience
@@ -166,6 +172,93 @@ router.put(
 
         const profile=await Profile.findOne({userid:req.user.id});
         profile.experience.unshift(newExp);
+        await profile.save(); //findAndUpdate
+
+        res.json(profile);//res.json({profile}); return & status opt
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+//DELETE api/profile/experience/:exp_id --- delete experience to authorised/loggedin user profile/experience
+//protected routes (authmw) //could be PUT req
+router.delete(
+    '/experience/:exp_id',
+    authmw,
+    async(req,res)=>{
+    try {
+        const profile=await Profile.findOne({userid:req.user.id});
+        //get remove index
+        const removeIndex=profile.experience.map(_exp=>_exp._id).indexOf(req.params.exp_id)//ok
+        //_exp=>_exp._id or _exp.id both ok
+
+        //const removeIndex=profile.experience.map((_exp,_index)=>{if(_exp._id==req.params.exp_id)return _index})[0] //ok
+        //(_exp.id==req.params.exp_id) or (_exp._id==req.params.exp_id) both ok
+
+        profile.experience.splice(removeIndex,1); //ok for these above
+
+       //profile.experience.forEach(_exp=>{if(_exp!=null&&_exp._id!=req.params.exp_id)return _exp}) //foreach not returning
+       //nothing changed
+       //let profExp=profile.experience
+       //profExp.forEach(_exp=>{if(_exp!=null&&_exp._id!=req.params.exp_id)return _exp})
+       //console.log('profExp ',profExp); //still nothing changed so cannot save or findandupdate
+       //profile.experience=profile.experience.map(_exp=>{if(_exp!=null&&_exp._id!=req.params.exp_id)return _exp}) 
+       //null //_exp._id!=req.params.exp_id or _exp.id!=req.params.exp_id -> get result -> null
+
+       //profile.experience.filter(_exp=>_exp!=null) //no changes to profile or profile.experience
+       //profile.experience=profile.experience.filter(_exp=>_exp!=null) //ok
+       //profile.experience=profile.experience.filter(_exp=>_exp.id!=req.params.exp_id) //ok
+       //_exp.id!=req.params.exp_id or _exp._id!=req.params.exp_id both ok
+
+        await profile.save();
+
+      //const profExp=profile.experience.map(_exp=>{if(_exp!=null&&_exp._id!=req.params.exp_id)return _exp})
+      /*const profExp=profile.experience.filter(_exp=>_exp.id!=req.params.exp_id) 
+      //_exp.id!=req.params.exp_id or _exp._id!=req.params.exp_id are ok
+      const newProf=await Profile.findOneAndUpdate(
+          {userid:req.user.id},{$set: {"experience": profExp}},{new:true}
+          ) //$set for multiple fields updates
+        res.json(newProf);*/ //ok
+
+        res.json(profile);//res.json({profile}); return & status opt
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+
+router.put(
+    '/experience/:exp_id',
+    authmw,
+    [
+        body('title','Title is required').not().isEmpty(),
+        body('company','Company is required').not().isEmpty(),
+        body('from','From Date is required').not().isEmpty()
+    ],
+    async(req,res)=>{
+        const errors=validationResult(req);
+        if(!errors.isEmpty)return res.status(400).json({errors:errors.array()});
+
+        const {title,company,location,from,to,current,description}=req.body;
+        const updatedExp={title,company,location,from,to,current,description};
+
+    try {
+
+        const profile=await Profile.findOne({userid:req.user.id});
+        /*const updatedProfExp=profile.experience.map(_exp=>{
+            if(_exp.id==req.params.exp_id)return updatedExp;
+            return _exp;
+        })
+        const updatedProf=await Profile.findOneAndUpdate({userid:req.user.id},{"experience":updatedProfExp},{new:true})
+        res.json(updatedProf);//res.json({updatedProf}); return & status opt
+        */ //ok
+
+        profile.experience=profile.experience.map(_exp=>{
+            if(_exp.id==req.params.exp_id)return updatedExp;
+            return _exp;
+        })
         await profile.save(); //findAndUpdate
 
         res.json(profile);//res.json({profile}); return & status opt
