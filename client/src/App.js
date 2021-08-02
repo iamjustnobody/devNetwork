4e700e01a7fb735104a79a8f1af9153a00631b13
randomcodedoodles <wmxyss@hotmail.com>
2021-08-03 00:13:02 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ const App=()=>
   <Router>
     <Fragment>
       <Navbar />
-      <Route exact path='/' component={Landing} />
+      <Route exact path='/' component={} />
     </Fragment>
   </Router>
   
