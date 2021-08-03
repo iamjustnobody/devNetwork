f7b98e524240070f95a50d02bc15ceacb42570b1
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-08-03 10:14:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,15 +11,12 @@ app.use(express.json({extended:false}));
 
 
 app.get('/',(req,res)=> res.send('API Running'));
-//app.use '/' -> '/xxx/xxxxx' etc; app.get '/' -> only '/'; 
-//use '/' so if '/xxx/xxxxx', not reaching following app.use/get/post etc
 
 //define routes
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
-//use -> use or get/post etc or route().get/post etc in api routes //get->not reaching apiroutes route path
 
 const PORT=process.env.PORT || 5000;
 
